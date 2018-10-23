a3b8597fe52a505736d4024d85997718e2de62e5
Andriy Sokirko <andriy.sokirko@spryker.com>
2018-10-23 13:43:30 +0300

    TE-184 add assets

--- config/install/heroku/run.sh
+++ config/install/heroku/run.sh
@@ -3,7 +3,7 @@
 set -xe
 
 if [ "$APPLICATION_NAME" == "Yves" ]; then
-    vendor/bin/install -r development-heroku -s twig-cache-warmup -s generate
+    vendor/bin/install -r development-heroku -s twig-cache-warmup
 else
-    vendor/bin/install -r development-heroku -s generate -s database-flush -s database-migrate -s queue -s navigation-cache-warmup
+    vendor/bin/install -r development-heroku -s database-flush -s database-migrate -s queue
 fi
