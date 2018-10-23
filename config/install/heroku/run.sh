1e0bfb8e1e5a93eaa223eb4975cf5774e03c98ae
Andriy Sokirko <andriy.sokirko@spryker.com>
2018-10-23 12:02:21 +0300

    TE-184 fix run script

--- config/install/heroku/run.sh
+++ config/install/heroku/run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
 set -xe
 
 if [ "$APPLICATION_NAME" == "Yves" ]; then
-    vendor/bin/install -r development-heroku -s cache -s generate -s yves-frontend
+    vendor/bin/install -r development-heroku -s twig-cache-warmup -s generate -s yves-frontend
 else
-    vendor/bin/install -r development-heroku -s generate -s database-flush -s database-migrate -s zed-frontend -s queue
+    vendor/bin/install -r development-heroku -s generate -s database-flush -s database-migrate -s zed-frontend -s queue -s navigation-cache-warmup
 fi
