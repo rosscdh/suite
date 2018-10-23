822bdc409201a7672da065cc6832340e3f6f87cc
Andriy Sokirko <andriy.sokirko@spryker.com>
2018-10-23 15:09:51 +0300

    TE-184 fix deploy

--- config/install/heroku/run.sh
+++ config/install/heroku/run.sh
@@ -5,5 +5,5 @@ set -xe
 if [ "$APPLICATION_NAME" == "Yves" ]; then
     vendor/bin/install -r development-heroku -s twig-cache-warmup
 else
-    vendor/bin/install -r development-heroku -s database-flush -s queue -s twig-cache-warmup
+    vendor/bin/install -r development-heroku -s database-flush -s queue -s twig-cache-warmup -s demodata
 fi
